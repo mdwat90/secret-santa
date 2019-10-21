3d410a51c2bef6aa0eac662c537e5315e4e62a31
mdwatson19 <mdwatson19@tribalapp.net>
2019-10-21 00:28:26 -0600

    Updates

---
+++ precache-manifest.2f081c1def3ae06ad3e10565e5e73957.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cb07371bb95e2e5233161f0516e702",
+    "url": "/secret-santa/index.html"
+  },
+  {
+    "revision": "5c9dbd17419f5217a866",
+    "url": "/secret-santa/static/js/2.98356842.chunk.js"
+  },
+  {
+    "revision": "b3234c79a2436900c960",
+    "url": "/secret-santa/static/js/main.a3cdf96d.chunk.js"
+  },
+  {
+    "revision": "043981a122cf2d4ca483",
+    "url": "/secret-santa/static/js/runtime~main.3d7d3162.js"
+  }
+]);
